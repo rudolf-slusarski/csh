5d9881e5e4e889b4227430e1de1cc1344db01158
rudolf <rudolf.slusarski@gmail.com>
2022-12-11 03:23:51 +0100

    loop + exit

--- main.c
+++ main.c
@@ -8,13 +8,25 @@ int main(int ac, char **argv)
     char *prompt = "please do $ ";
     char *lineptr;
     size_t n = 0;
+    ssize_t nchars;
 
-    (void)ac; (void)argv;
+    (void)ac;
+    (void)argv;
 
-    printf("%s", prompt);
-    getline(&lineptr, &n, stdin);
-    printf("%s\n", lineptr);
+    while (1)
+    {
+        printf("%s", prompt);
+        nchars = getline(&lineptr, &n, stdin);
 
-    free(lineptr);
+        if (nchars == -1)
+        {
+            printf("exiting\n");
+            return -1;
+        }
+
+        printf("%s", lineptr);
+        free(lineptr);
+    }
+
     return 0;
 }
